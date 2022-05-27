536fd422676fe9ca30c547023e0600c660f01f38
brianschnee <brian.a.schnee@gmail.com>
2022-05-27 18:26:58 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+document.getElementById('taskSearchButton').addEventListener('click', getTodos);
+
+async function getTodos() {
+	const taskName = document.getElementById('taskSearch').value;
+
+	try {
+		const res = await fetch(`/api?todo=${taskName}`);
+		const data = await res.json();
+		console.log(data);
+
+		document.getElementById('title').innerText = data.title;
+		document.getElementById('content').innerText = data.content;
+	} catch (err) {
+		console.log(`err: ${err}`);
+	}
+}
---
+++ todo.js
@@ -0,0 +1,22 @@
+exports.todo = [
+	{
+		title: 'buy milk',
+		content: 'go to the store and buy some milk',
+	},
+	{
+		title: 'do my homework',
+		content: 'do math problems from 1-10 for friday',
+	},
+	{
+		title: 'do codewars',
+		content: 'work on that 5 kyu',
+	},
+	{
+		title: 'networking',
+		content: 'try to get at least 2 coffee chats for the week',
+	},
+	{
+		title: 'example',
+		content: 'try to get at least 2 coffee chats for the week',
+	},
+];
